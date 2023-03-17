c74695d06dbad5843bf32ec5f96f9fabdddd01a7
paulobdelacruz16 <paulobdelacruz16@gmail.com>
2023-03-18 00:15:31 +0800

    added post validation

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,24 +1,29 @@
-
-const Book = require('../models/books');
+const { Book, validateBook } = require("../models/books");
 
 const routes = (app) => {
-app.route('/api/books').post((req,res) =>{
-    console.log('hey', req.body);
-   var book = new Book({
-        name:req.body.bookName,
-        author:{
-            name:req.body.authorName,
-            age:req.body.authorAge
-        },
-        genre:req.body.genre
+  app.route("/api/books").post(async (req, res) => {
+    console.log("hey", req.body);
+    const error = await validateBook(req.body);
+    console.log('message123', error);
+    if(error.message) res.status(400).send(error.message)
+    const book = new Book({
+      name: req.body.bookName,
+      author: {
+        name: req.body.authorName,
+        age: req.body.authorAge,
+      },
+      genre: req.body.genre,
     });
-    book.save().then(book =>{
-        res.send(book)
-    }).catch(error => {
-        console.log('error1', error);
+    book
+      .save()
+      .then((book) => {
+        res.send(book);
+      })
+      .catch((error) => {
+        console.log("error1", error);
         res.status(500).send("Book was not stored in DB");
-    });
-});
-}
+      });
+  });
+};
 
 export default routes;
