ae81f85593dc054032bbfc9b756b2f78f6870802
paulobdelacruz16 <paulobdelacruz16@gmail.com>
2023-03-18 02:08:31 +0800

    added winston colorized debugger

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
-require('dotenv').config();
+import winston from "winston";
+require("dotenv").config();
 // import routes from "./src/routes/books";
 
 import bodyParser from "body-parser";
 
 import routes from "./src/routes/books";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 //bodyparser setup
 app.use(bodyParser.json());
@@ -20,13 +21,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //routes
 // app.use('/', routes);
 
+const logger = winston.createLogger({
+  level: "info",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize({
+          all: true,
+        })
+      )
+    }),
+    new winston.transports.File({filename: 'error.log', level: 'error'})
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'exceptions.log'})
+  ]
+});
 
 // mongoose connection
-mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true}).then(() => {
-    console.log("connected to mongodb atlas");
-}).catch(error => {
-    console.log('Mongoose Connection fail', error)
-});
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    logger.info("connected to mongodb atlas")
+  })
+  .catch((error) => {
+    logger.log("error", error.message)
+  });
 
 //bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,4 +61,6 @@ app.get("/", (req, res) =>
   res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  logger.info("Your server is running on port ${PORT}")
+});
